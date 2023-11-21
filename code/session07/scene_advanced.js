330f24dec5ca74eef22aab79a5364f8aec6ffa2e
Malte Hillebrand <maltehillebrand96@gmail.com>
2023-11-21 11:44:44 +0100

    edited code syntax to match rest (use of const)

--- code/session07/scene_advanced.js
+++ code/session07/scene_advanced.js
@@ -8,17 +8,17 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // SCENE
-let scene = new THREE.Scene();
+const scene = new THREE.Scene();
 scene.background = new THREE.Color('#a6a6a6');
 scene.fog = new THREE.FogExp2(scene.background, 0.02);
 
 // CAMERA
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 10;
 
 // RENDERER
 const canvas = document.querySelector("#canvasThree");
-let renderer = new THREE.WebGLRenderer({ canvas, alpha: true, antialias: true });
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -33,15 +33,15 @@ window.addEventListener('resize', () => {
 });
 
 // CONTROLS FOR NAVIGATION
-let controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // LIGHTING
 // AMBIENT
-let ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
 scene.add(ambientLight);
 
 // POINT LIGHT
-let pointLight = new THREE.PointLight(0xc9efff, 25);
+const pointLight = new THREE.PointLight(0xc9efff, 25);
 pointLight.position.set(0, 4, 2);
 pointLight.castShadow = true;
 pointLight.shadow.mapSize.width = 1024;
@@ -57,25 +57,25 @@ scene.add(pointLightHelper);
 
 // GEOMETRY
 // PLANE
-let planeGeometry = new THREE.PlaneGeometry(300, 300);
-let planeMaterial = new THREE.MeshPhongMaterial({ color: '#9c9595', depthWrite: false });
-let planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+const planeGeometry = new THREE.PlaneGeometry(300, 300);
+const planeMaterial = new THREE.MeshPhongMaterial({ color: '#9c9595', depthWrite: false });
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 planeMesh.rotation.x = -Math.PI / 2;
 planeMesh.position.y = -10;
 planeMesh.receiveShadow = true;
 scene.add(planeMesh);
 
 // ICOSAHEDRON
-let geometry = new THREE.IcosahedronGeometry(1.5, 0); //radius, detail
-let material = new THREE.MeshPhongMaterial({ color: 0xaa5e82, shininess: 0.5 });
-let icosa = new THREE.Mesh(geometry, material);
+const geometry = new THREE.IcosahedronGeometry(1.5, 0); //radius, detail
+const material = new THREE.MeshPhongMaterial({ color: 0xaa5e82, shininess: 0.5 });
+const icosa = new THREE.Mesh(geometry, material);
 icosa.castShadow = true;
 scene.add(icosa);
 
 // ICOSAHEDRON 2
-let icosaGeometry_2 = new THREE.IcosahedronGeometry(2.0, 0); //radius, detail
-let material_2 = new THREE.MeshPhongMaterial({ color: 0x2d2d2d, wireframe: true });
-let icosa_2 = new THREE.Mesh(icosaGeometry_2, material_2);
+const icosaGeometry_2 = new THREE.IcosahedronGeometry(2.0, 0); //radius, detail
+const material_2 = new THREE.MeshPhongMaterial({ color: 0x2d2d2d, wireframe: true });
+const icosa_2 = new THREE.Mesh(icosaGeometry_2, material_2);
 scene.add(icosa_2);
 
 
